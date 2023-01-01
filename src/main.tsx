650f4f138f4cb575b1d9e6ed5be601ff9068a11e
bayanKodehode <116548227+BayanKodehode@users.noreply.github.com>
2023-01-02 00:59:09 +0100

    LGTM

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import BoxWrapper from './BoxWrapper';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BoxWrapper/>
   </React.StrictMode>,
 )

5275c25c138715a0406eac03fd35788311993c29
bayanKodehode <116548227+BayanKodehode@users.noreply.github.com>
2023-01-03 01:30:37 +0100

    LGTM for now

--- src/components/bot.tsx
+++ src/components/bot.tsx
@@ -13,52 +13,47 @@ export const Bot = ({ greeting }: IBot) => {
   const [name, setName] = useState<string>("");
   const [visibility, setVisibility] = useState<boolean>(false);
   const { riddles, jokes, fetchRiddles, fetchJokes } = useContext(DataContext);
-
-  const onRiddleRequest = async () => {
-    try {
-      await fetchRiddles();
-      if (riddles.length > 0) {
-        setConversation([
-          <>
-            {riddles[0].title}
-            <br />
-            <br />
-            {riddles[0].question}
-            <br />
-            <br />
-            {riddles[0].answer}
-          </>,
-        ]);
-        console.log(riddles);
-      } else {
-        // handle case where riddles array is empty
-        // need to find a solution to this later
-        setConversation([
-          "Sorry, but don't give up! Keep trying and maybe you'll get lucky.",
-        ]);
-      }
-    } catch (error) {
-      console.log(error);
+  
+  
+ const onRiddleRequest = async () => {
+  try {
+    await fetchRiddles();
+    if (riddles.length > 0) {
+      setConversation([
+        ` Title : ${riddles[0].title} \n\n
+          , and the riddle is : ${riddles[0].question}
+          , the answer is :${riddles[0].answer} `
+      ]);
+      console.log(riddles);
+    } else {
+      // handle case where riddles array is empty
+      // need to find a solution to this later
+      setConversation([
+        "Sorry, but don't give up! Keep trying and maybe you'll get lucky.",
+      ]);
     }
-  };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  const onJokeRequest = async () => {
-    try {
-      await fetchJokes();
-      if (jokes.length > 0) {
-        setConversation([<>{jokes[0].joke}</>]);
-        console.log(jokes);
-      } else {
-        // handle case where jokes array is empty
-        // need to find a solution to this later
-        setConversation([
-          "Don't worry though, just click again and maybe I'll have a joke for you next time!",
-        ]);
-      }
-    } catch (error) {
-      console.log(error);
+const onJokeRequest = async () => {
+  try {
+    await fetchJokes();
+    if (jokes.length > 0) {
+      setConversation([`${jokes[0].joke}`]);
+      console.log(jokes);
+    } else {
+      // handle case where jokes array is empty
+      // need to find a solution to this later
+      setConversation([
+        "Don't worry though, just click again and maybe I'll have a joke for you next time!",
+      ]);
     }
-  };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
   const toggleVisibility = () => {
     setVisibility(!visibility);
--- src/context/DataProvider.tsx
+++ src/context/DataProvider.tsx
@@ -1,61 +1,138 @@
-import * as React from "react";
+import { createContext, useContext, useState, useEffect, useCallback } from "react";
 
-type DataContextType = {
-  riddles: string[];
-  jokes: string[];
-  fetchRiddles: () => void;
-  fetchJokes: () => void;
-};
+// Create an interface for the riddle data type
+interface IRiddle {
+  title: string;
+  question: string;
+  answer: string;
+}
 
-export const DataContext = React.createContext<DataContextType>({
+// Create an interface for the joke data type
+interface IJoke {
+  joke: string;
+}
+
+// Create an interface for the context data type
+interface IDataContext {
+  riddles: IRiddle[];
+  jokes: IJoke[];
+  fetchRiddles: () => Promise<void>;
+  fetchJokes: () => Promise<void>;
+}
+
+// Create the context object with the initial data values
+export const DataContext = createContext<IDataContext>({
   riddles: [],
   jokes: [],
-  fetchRiddles: () => {},
-  fetchJokes: () => {},
+  fetchRiddles: () => Promise.resolve(),
+  fetchJokes: () => Promise.resolve(),
 });
 
-type DataProviderProps = {
-  children: React.ReactNode;   // need to learn more about ReactNode later on
+// The provider component that will wrap around the entire app
+export const DataProvider = ({ children }: { children: React.ReactNode }) => {
+  const [riddles, setRiddles] = useState<IRiddle[]>([]);
+  const [jokes, setJokes] = useState<IJoke[]>([]);
+
+  const fetchRiddles = useCallback(async () => {
+        try {
+          const response = await fetch("https://api.api-ninjas.com/v1/riddles");
+          const riddles = await response.json();
+          setRiddles(riddles);
+        } catch (error) {
+          console.log(error);
+        }
+      }, []);
+
+      const fetchJokes = useCallback(async () => {
+            const limit = 10;
+            try {
+              const response = await fetch("https://api.api-ninjas.com/v1/jokes?limit='" + limit,
+              {
+                headers: {
+                        "X-Api-Key": "0DhBnOBWv10+HOeWsN0T1w==KEfK5ymNm4BcPBud",
+                      },
+              });
+              const jokes = await response.json();
+              setJokes(jokes);
+            } catch (error) {
+              console.log(error);
+            }
+          }, []);
+
+  useEffect(() => {
+    fetchRiddles();
+    fetchJokes();
+  }, []);
+
+  return (
+    <DataContext.Provider value={{ riddles, jokes, fetchRiddles, fetchJokes }}>
+      {children}
+    </DataContext.Provider>
+  );
 };
 
-export const DataProvider = ({ children }: DataProviderProps) => {
-  const [riddles, setRiddles] = React.useState<string[]>([]);
-  const [jokes, setJokes] = React.useState<string[]>([]);
+// A hook to make it easier to access the context data within a component
+export const useData = () => useContext(DataContext);
+
+
+// import * as React from "react";
+
+// type DataContextType = {
+//   riddles: string[];
+//   jokes: string[];
+//   fetchRiddles: () => void;
+//   fetchJokes: () => void;
+// };
+
+// export const DataContext = React.createContext<DataContextType>({
+//   riddles: [],
+//   jokes: [],
+//   fetchRiddles: () => {},
+//   fetchJokes: () => {},
+// });
+
+// type DataProviderProps = {
+//   children: React.ReactNode;   // need to learn more about ReactNode later on
+// };
+
+// export const DataProvider = ({ children }: DataProviderProps) => {
+//   const [riddles, setRiddles] = React.useState<string[]>([]);
+//   const [jokes, setJokes] = React.useState<string[]>([]);
  
-  const fetchRiddles = React.useCallback(async () => {
-    try {
-      const response = await fetch("https://api.api-ninjas.com/v1/riddles");
-      const riddles = await response.json();
-      setRiddles(riddles);
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
+//   const fetchRiddles = React.useCallback(async () => {
+//     try {
+//       const response = await fetch("https://api.api-ninjas.com/v1/riddles");
+//       const riddles = await response.json();
+//       setRiddles(riddles);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }, []);
   
-  const fetchJokes = React.useCallback(async () => {
-    const limit = 10;
-    try {
-      const response = await fetch("https://api.api-ninjas.com/v1/jokes?limit='" + limit,
-      {
-        headers: {
-                "X-Api-Key": "0DhBnOBWv10+HOeWsN0T1w==KEfK5ymNm4BcPBud",
-              },
-      });
-      const jokes = await response.json();
-      setJokes(jokes);
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
+//   const fetchJokes = React.useCallback(async () => {
+//     const limit = 10;
+//     try {
+//       const response = await fetch("https://api.api-ninjas.com/v1/jokes?limit='" + limit,
+//       {
+//         headers: {
+//                 "X-Api-Key": "0DhBnOBWv10+HOeWsN0T1w==KEfK5ymNm4BcPBud",
+//               },
+//       });
+//       const jokes = await response.json();
+//       setJokes(jokes);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }, []);
   
-  const value: DataContextType = {
-    riddles,
-    jokes,
-    fetchRiddles,
-    fetchJokes,
-  };
+//   const value: DataContextType = {
+//     riddles,
+//     jokes,
+//     fetchRiddles,
+//     fetchJokes,
+//   };
 
-  return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
-};
+//   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
+// };
 
-export default DataProvider;
+// export default DataProvider;
